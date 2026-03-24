a37165ef48f6db5eef1800c1d28bd98103e9af5d
maksim92982 <ma89spb@gmail.com>
2026-03-24 08:47:14 +0300

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -5,7 +5,8 @@ export default function handler(req) {
     return new Response('Method not allowed', { status: 405 });
   }
 
-  const baseUrl = 'https://program-site-ebon.vercel.app/';
+  // БЕЗ пробелов в конце!
+  const baseUrl = 'https://program-site-ebon.vercel.app';
   
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
